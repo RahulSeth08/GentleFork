ae6494cca46b6007cf40741f7c3b41c11f06b63b
Madhav <ms99194@gmail.com>
2024-05-18 11:34:22 +0530

    NewReleases

--- src/components/NewReleases.js
+++ src/components/NewReleases.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import  "../styles/NewReleases.css"
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
 
 const NewReleases = () => {
   return (
@@ -24,6 +28,46 @@ const NewReleases = () => {
             <h3>Pet Stairs with 4 Anti-Slip Steps</h3>
             <p>Make Your Pet's Routine and Life Easier with Portable Stairs and Ramps!</p>
             <a href="#" className="btn">Buy now</a>
+            {/* <Stack direction="row" spacing={2}> */}
+            
+{/* <div class="products">
+    <div class="box-container">
+        <div class="box">
+            <div class="content">
+                <h2>Product 1</h2>
+                <p>Description of product 1</p>
+                <div class="price">$20</div>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+
+<div class="cart-icon">
+    <img src="cart-icon.png" alt="Cart"/>
+</div>
+
+
+<div class="cart-page hidden">
+    <h2>Items in Cart</h2>
+    <ul class="cart-items">
+        
+    </ul>
+</div> */}
+
+      {/* <Button classname="box" variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </Stack> */}
+        
+
+       
+
         </div>
     </div>
 
